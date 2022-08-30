80502dfc9b51feee7e0308bca523a0b65ef09c25
DESKTOP-OVMJL3E\aling <alingavrilescu@yahoo.com>
2022-08-30 15:24:09 +0300

    import some pack

--- EmployeeEvaluation/Controllers/UsersController.cs
+++ EmployeeEvaluation/Controllers/UsersController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
