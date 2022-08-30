0e2a50a3a834a0d8849b2e25505b55182996563a
DESKTOP-OVMJL3E\aling <alingavrilescu@yahoo.com>
2022-08-30 17:20:17 +0300

    Create repos and made the initial migration

---
+++ EmployeeEvaluation.DataAccess.EntityFramework/ProjectRepository.cs
@@ -0,0 +1,33 @@
+﻿using EmployeeEvaluation.DataAccess.Abstractions;
+using EmployeeEvaluation.DataAccess.Model;
+
+namespace EmployeeEvaluation.DataAccess.EntityFramework
+{
+    public class ProjectRepository : IProjectRepository
+    {
+        public Project Add(Project toAdd)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Project> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Project GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Project Update(Project toUpdate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
