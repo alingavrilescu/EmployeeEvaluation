df720025767dc2358e784722ba1ce4fce3174004
SinkyZ <SinkyZ@SINKY>
2022-08-31 14:54:22 +0300

    Add controller for roles

---
+++ EmployeeEvaluation/Controllers/RolesController.cs
@@ -0,0 +1,38 @@
+﻿using EmployeeEvaluation.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EmployeeEvaluation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RolesController : ControllerBase
+    {
+        private RoleManager<IdentityRole> roleManager;
+        public RolesController(RoleManager<IdentityRole> _roleManager)
+        {
+            roleManager = _roleManager;
+        }
+
+        // GET: api/<RolesController>
+        [HttpGet]
+        public async Task<IActionResult> GetAllRoles()
+        {
+            var roles = await roleManager.Roles.ToListAsync();
+            return Ok(roles);
+        }
+
+        // GET api/<RolesController>/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetRoleById(string id)
+        {
+            var role = await roleManager.FindByIdAsync(id);
+            return Ok(role); 
+        }
+
+    }
+}
