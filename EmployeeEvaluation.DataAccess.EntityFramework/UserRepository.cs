31cc3652dea98132e1b673e130e96b0a36daab35
Raluca0Bratan <raluca.bratan@gmail.com>
2022-09-01 14:05:15 +0300

    User repository

---
+++ EmployeeEvaluation.DataAccess.EntityFramework/UserRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeEvaluation.DataAccess.EntityFramework
+{
+    public class UserRepository
+    {
+        public readonly UserDbContext dbContext;
+        public UserRepository(UserDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+         public User Add(User toAdd)
+        {
+            var user = dbContext.Set<User>.Add(toAdd);
+            dbContext.SaveChanges();
+            return user;
+        }
+        public IEnumerable<User> GetUsers()
+        {
+            var users = dbContext.Set<User>()
+                .ToList;
+            return users;
+        }
+    }
+}
