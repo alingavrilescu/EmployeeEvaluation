329979730ca4e66a859a51e6b8162601dadca345
Raluca0Bratan <raluca.bratan@gmail.com>
2022-09-01 14:21:04 +0300

    Completed user repository

--- EmployeeEvaluation.DataAccess.EntityFramework/UserRepository.cs
+++ EmployeeEvaluation.DataAccess.EntityFramework/UserRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EmployeeEvaluation.DataAccess.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,22 +9,42 @@ namespace EmployeeEvaluation.DataAccess.EntityFramework
 {
     public class UserRepository
     {
-        public readonly UserDbContext dbContext;
-        public UserRepository(UserDbContext dbContext)
+        public readonly ProjectDbcontext dbContext;
+        public UserRepository(ProjectDbcontext dbContext)
         {
             this.dbContext = dbContext;
         }
          public User Add(User toAdd)
         {
-            var user = dbContext.Set<User>.Add(toAdd);
+            var user = dbContext.Set<User>().Add(toAdd);
             dbContext.SaveChanges();
-            return user;
+            return user.Entity;
         }
         public IEnumerable<User> GetUsers()
         {
-            var users = dbContext.Set<User>()
-                .ToList;
-            return users;
+            return dbContext.Set<User>()
+                .ToList();
+        }
+        public User GetUserById(Guid id)
+        {
+            var user = dbContext.Set<User>().Where(u => u.Id == id)
+                                          .First();
+            return user;
+        }
+
+        public void DeleteById(Guid id)
+        {
+            var toDelete = GetUserById(id);
+            dbContext.Set<User>()
+                     .Remove(toDelete);
+            dbContext.SaveChanges();
+        }
+
+        public User Update(User toUpdate)
+        {
+            dbContext.Set<User>().Update(toUpdate);
+            dbContext.SaveChanges();
+            return toUpdate;
         }
     }
 }
