60d5bfe337e529bd146a6cd099289505f52c335b
SinkyZ <SinkyZ@SINKY>
2022-09-02 14:24:19 +0300

    Modify UserRepository and add UserService

--- EmployeeEvaluation.DataAccess.EntityFramework/UserRepository.cs
+++ EmployeeEvaluation.DataAccess.EntityFramework/UserRepository.cs
@@ -1,9 +1,6 @@
 ﻿using EmployeeEvaluation.DataAccess.Model;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace EmployeeEvaluation.DataAccess.EntityFramework
 {
@@ -20,31 +17,26 @@ namespace EmployeeEvaluation.DataAccess.EntityFramework
             dbContext.SaveChanges();
             return user.Entity;
         }
-        public IEnumerable<User> GetUsers()
+        public IEnumerable<User> GetAll()
         {
-            return dbContext.Set<User>()
-                .ToList();
+            return dbContext.Set<User>().ToList();
         }
-        public User GetUserById(Guid id)
+        public User GetById(Guid id)
         {
-            var user = dbContext.Set<User>().Where(u => u.Id == id)
-                                          .First();
-            return user;
+            var userToReturn = dbContext.Set<User>().Where(u => u.Id == id).Include(p => p.Project).FirstOrDefault();
+            if(userToReturn == null)
+            {
+                throw new KeyNotFoundException("User not found");
+            }                               
+            return userToReturn;
         }
 
         public void DeleteById(Guid id)
         {
-            var toDelete = GetUserById(id);
-            dbContext.Set<User>()
-                     .Remove(toDelete);
+            var toDelete = GetById(id);
+            dbContext.Set<User>().Remove(toDelete);
             dbContext.SaveChanges();
         }
 
-        public User Update(User toUpdate)
-        {
-            dbContext.Set<User>().Update(toUpdate);
-            dbContext.SaveChanges();
-            return toUpdate;
-        }
     }
 }
