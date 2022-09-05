56055231621c744779587d6a715d5c5c2833a662
Alexandra Badaranescu <badaranescualexandra@yahoo.com>
2022-09-05 14:23:14 +0300

    Created EvaluationForm, FormSection si FormDbContext

---
+++ EmployeeEvaluation.DataAccess.EntityFramework/EvaluationFormDbContext.cs
@@ -0,0 +1,13 @@
+﻿using EmployeeEvaluation.DataAccess.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeEvaluation.DataAccess.EntityFramework
+{
+    public class EvaluationFormDbContext: DbContext
+    {
+        public EvaluationFormDbContext(DbContextOptions<EvaluationFormDbContext> options) : base(options)
+        {
+        }
+
+    }
+}
---
+++ EmployeeEvaluation.DataAccess.Model/FormSection.cs
@@ -0,0 +1,13 @@
+﻿
+namespace EmployeeEvaluation.DataAccess.Model
+{
+    public class FormSection
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Guid EvaluationFormId { get; set; }
+
+
+    }
+}
