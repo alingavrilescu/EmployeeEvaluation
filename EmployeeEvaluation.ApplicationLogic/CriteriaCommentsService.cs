f1be8ab3cbde6e49837e57e4477e0569e2f7db19
SinkyZ <SinkyZ@SINKY>
2022-09-05 14:35:49 +0300

    Created services, repos and models for FormCriteria and CriteriaComments.

---
+++ EmployeeEvaluation.ApplicationLogic/CriteriaCommentsService.cs
@@ -0,0 +1,50 @@
+﻿using EmployeeEvaluation.DataAccess.Abstractions;
+using EmployeeEvaluation.DataAccess.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeEvaluation.ApplicationLogic
+{
+    public class CriteriaCommentsService
+    {
+
+        private readonly ICriteriaCommentsRepository _criteriaCommentsRepository;
+
+        public CriteriaCommentsService(ICriteriaCommentsRepository criteriaCommentsRepository)
+        {
+            this._criteriaCommentsRepository = criteriaCommentsRepository;
+        }
+
+        public CriteriaComments GetCriteriaCommentsById(Guid id)
+        {
+            return this._criteriaCommentsRepository.GetById(id);
+        }
+
+        public IEnumerable<CriteriaComments> GetCriteriaComments()
+        {
+            return this._criteriaCommentsRepository.GetAll();
+        }
+
+        public CriteriaComments AddCriteriaComments(CriteriaComments toAdd)
+        {
+            return _criteriaCommentsRepository.Add(toAdd);
+        }
+
+        public void DeleteCriteriaComments(Guid id)
+        {
+            this._criteriaCommentsRepository.DeleteById(id);
+        }
+
+        public CriteriaComments UpdateCriteriaComments(CriteriaComments criteriaComments)
+        {
+            var existingCriteriaComments = _criteriaCommentsRepository.GetById(criteriaComments.Id);
+            existingCriteriaComments.Comment = criteriaComments.Comment;
+            existingCriteriaComments.Attachment = criteriaComments.Attachment;
+            return existingCriteriaComments;
+        }
+
+    }
+}
---
+++ EmployeeEvaluation.DataAccess.EntityFramework/FormCriteriaRepository.cs
@@ -0,0 +1,54 @@
+﻿using EmployeeEvaluation.DataAccess.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeEvaluation.DataAccess.EntityFramework
+{
+    public class FormCriteriaRepository
+    {
+
+        public readonly FormDbcontext dbContext;
+        public FormCriteriaRepository(FormDbcontext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public FormCriteria Add(FormCriteria toAdd)
+        {
+            var formCriteria = dbContext.Set<FormCriteria>().Add(toAdd);
+            dbContext.SaveChanges();
+            return formCriteria.Entity;
+        }
+        public IEnumerable<FormCriteria> GetAll()
+        {
+            return dbContext.Set<FormCriteria>().ToList();
+        }
+        //public FormCriteria GetById(Guid id)
+        //{
+        //    var formCriteriaToReturn = dbContext.Set<FormCriteria>().Where(c => c.Id == id).Include(s => s.FormSection).FirstOrDefault();
+        //    if (formCriteriaToReturn == null)
+        //    {
+        //        throw new KeyNotFoundException("Form criteria not found");
+        //    }
+        //    return formCriteriaToReturn;
+        //}
+
+        //public void DeleteById(Guid id)
+        //{
+        //    var toDelete = GetById(id);
+        //    dbContext.Set<FormCriteria>().Remove(toDelete);
+        //    dbContext.SaveChanges();
+        //}
+
+        public FormCriteria Update(FormCriteria toUpdate)
+        {
+            dbContext.Set<FormCriteria>().Update(toUpdate);
+            dbContext.SaveChanges();
+            return toUpdate;
+        }
+    }
+}
