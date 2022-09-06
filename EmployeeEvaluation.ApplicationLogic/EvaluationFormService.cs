4f5d473b493d75a10edf98ec5a00e851bae6102e
SinkyZ <SinkyZ@SINKY>
2022-09-06 13:54:51 +0300

    Create EvaluationForm repository and services.

---
+++ EmployeeEvaluation.ApplicationLogic/EvaluationFormService.cs
@@ -0,0 +1,35 @@
+﻿using EmployeeEvaluation.DataAccess.Abstractions;
+using EmployeeEvaluation.DataAccess.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeEvaluation.ApplicationLogic
+{
+    public class EvaluationFormService
+    {
+        private readonly IEvaluationFormRepository _evaluationFormRepository;
+
+        public EvaluationFormService(IEvaluationFormRepository evaluationFormRepository)
+        {
+            this._evaluationFormRepository = evaluationFormRepository;
+        }
+
+        public EvaluationForm GetEvaluationFormById(Guid id)
+        {
+            return this._evaluationFormRepository.GetById(id);
+        }
+
+        public IEnumerable<EvaluationForm> GetEvaluationForm()
+        {
+            return this._evaluationFormRepository.GetAll();
+        }
+
+        public EvaluationForm AddEvaluationForm(EvaluationForm toAdd)
+        {
+            return _evaluationFormRepository.AddEvaluationForm(toAdd);
+        }
+    }
+}
