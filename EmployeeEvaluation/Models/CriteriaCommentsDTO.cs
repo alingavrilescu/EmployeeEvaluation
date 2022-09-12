0a4769a96923a15fd9a4bda47024ba85fcc7dffe
SinkyZ <SinkyZ@SINKY>
2022-09-12 13:43:48 +0300

    Update methods in controller and add DTO's for FormSection, FormCriteria, CriteriaComments.

---
+++ EmployeeEvaluation/Models/CriteriaCommentsDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace EmployeeEvaluation.Models
+{
+    public class CriteriaCommentsDTO
+    {
+        public string Comment { get; set; }
+        public string Attachment { get; set; }
+        public Guid CriteriaId { get; set; }
+    }
+}
