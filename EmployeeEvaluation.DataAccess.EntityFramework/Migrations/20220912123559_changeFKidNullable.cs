167c463872458f65adf59bae740296ff1b467a7a
DESKTOP-OVMJL3E\aling <alingavrilescu@yahoo.com>
2022-09-12 15:36:42 +0300

    Changed the link between department and projects

---
+++ EmployeeEvaluation.DataAccess.EntityFramework/Migrations/20220912123559_changeFKidNullable.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmployeeEvaluation.DataAccess.EntityFramework.Migrations
+{
+    public partial class changeFKidNullable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Projects_Departments_DepartmentId",
+                table: "Projects");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "DepartmentId",
+                table: "Projects",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Projects_Departments_DepartmentId",
+                table: "Projects",
+                column: "DepartmentId",
+                principalTable: "Departments",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Projects_Departments_DepartmentId",
+                table: "Projects");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "DepartmentId",
+                table: "Projects",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Projects_Departments_DepartmentId",
+                table: "Projects",
+                column: "DepartmentId",
+                principalTable: "Departments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
