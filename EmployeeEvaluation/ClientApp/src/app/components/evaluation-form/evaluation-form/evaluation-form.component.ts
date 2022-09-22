8a8b28ca396df7d24cf6a6acbd8038f4b38fd392
SinkyZ <SinkyZ@SINKY>
2022-09-22 15:03:21 +0300

    Update evaluation form.

--- EmployeeEvaluation/ClientApp/src/app/components/evaluation-form/evaluation-form/evaluation-form.component.ts
+++ EmployeeEvaluation/ClientApp/src/app/components/evaluation-form/evaluation-form/evaluation-form.component.ts
@@ -4,9 +4,6 @@ import { Guid } from 'guid-typescript';
 import { Observable, Subscription } from 'rxjs';
 import { EvaluationForm } from 'src/app/models/evaluation-form.model';
 import { EvaluationFormService } from 'src/app/services/evaluation-form.service';
-import { FormSection } from 'src/app/models/form-section.model';
-import { FormCriteria } from 'src/app/models/form-criteria.model';
-import { CriteriaComments } from 'src/app/models/criteria-comments.model';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -20,24 +17,13 @@ export class EvaluationFormComponent implements OnInit, OnDestroy {
   deleteSubscription!: Subscription;
 
   evaluationForm!: EvaluationForm;
-  formSectionList: FormSection[] = [];
-  formCriteriaList: FormCriteria[] = [];
-  criteriaCommentsList: CriteriaComments[] = [];
-
-  evaluationFormId: any;
-  evaluationFormName = "";
-  evaluationFormType = "";
   userId: any;
-
 
   ngOnInit(): void {
     this.activatedRoute.paramMap.subscribe((params) => {
       this.userId = params.get('id');
     });
     this.refreshEvaluationFormList();
-    this.refreshFormSectionList();
-    this.refreshFormCriteriaList();
-    this.refreshCriteriaComments();
   }
 
   ngOnDestroy(): void {
@@ -47,35 +33,7 @@ export class EvaluationFormComponent implements OnInit, OnDestroy {
   refreshEvaluationFormList() {
     this.evaluationFormService.getEvaluationForms(this.userId).subscribe(data => {
       this.evaluationForm = data;
+      console.log(this.evaluationForm.formSections)
     })
-  }
-
-  refreshFormSectionList() {
-    this.evaluationFormService.getFormSections().subscribe(data => {
-      this.formSectionList = data;
-      this.formSectionList.filter(section => section.evaluationFormId === this.evaluationForm.id)
-      console.log(this.formSectionList)
-    })
-  }
-
-  refreshFormCriteriaList() {
-    this.evaluationFormService.getFormCriterias().subscribe(data => {
-      this.formCriteriaList = data;
-    })
-  }
-
-  refreshCriteriaComments() {
-    this.evaluationFormService.getCriteriaComments().subscribe(data => {
-      this.criteriaCommentsList = data;
-    })
-  }
-
-  addEvaluationForm() {
-    var temp = {
-      name: this.evaluationFormName,
-      type: this.evaluationFormType,
-      userId: this.userId
-    };
-    this.evaluationFormService.createEvaluationForm(temp).subscribe(() => { this.refreshEvaluationFormList(); });
   }
 }
