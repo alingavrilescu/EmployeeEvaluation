7b6fee16e6d294485e191fdef6be7b28e4b32b99
Raluca0Bratan <raluca.bratan@gmail.com>
2022-09-28 12:05:18 +0300

    code refactorization form-templates

---
+++ EmployeeEvaluation/ClientApp/src/app/software-developer-type.spec.ts
@@ -0,0 +1,7 @@
+import { SoftwareDeveloperType } from './software-developer-type';
+
+describe('SoftwareDeveloperType', () => {
+  it('should create an instance', () => {
+    expect(new SoftwareDeveloperType()).toBeTruthy();
+  });
+});
