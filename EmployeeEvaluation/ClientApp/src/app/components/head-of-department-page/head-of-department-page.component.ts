1b1cf1eb82066d50fd815ecb5b5b3775a0d50a4d
Catalin Sbora <catalin.sbora@gmail.com>
2022-09-30 13:41:12 +0300

    head of department home page

--- EmployeeEvaluation/ClientApp/src/app/components/head-of-department-page/head-of-department-page.component.ts
+++ EmployeeEvaluation/ClientApp/src/app/components/head-of-department-page/head-of-department-page.component.ts
@@ -1,7 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { Guid } from 'guid-typescript';
+import { Observable } from 'rxjs/internal/Observable';
+import { filter, map, tap, toArray } from 'rxjs/operators';
 import { AuthorizeService } from 'src/api-authorization/authorize.service';
+import { Department } from 'src/app/models/department.model';
+import { FormTemplate } from 'src/app/models/form-template.model';
+import { Project } from 'src/app/models/project.model';
 import { UserDTO } from 'src/app/models/users.model';
+import { DepartmentsService } from 'src/app/services/departments.service';
+import { FormTemplateService } from 'src/app/services/form-template.service';
+import { ProjectsService } from 'src/app/services/projects.service';
 import { UsersService } from 'src/app/services/users.service';
 interface City {
   name: string;
@@ -16,11 +24,18 @@ interface City {
 
 export class HeadOfDepartmentPageComponent implements OnInit {
   headOfDepartment!: UserDTO;
-  departmentId!: Guid;
+  departmentId!: Observable<string>; 
+  department!: Observable<Department>; 
+  formTemplates!: Observable<FormTemplate[]>;
+  projects!: Observable<Project[]>;
+  members!: Observable<UserDTO[]>;
   cities: City[];
   constructor(
     private authorizeService: AuthorizeService,
-    private userService: UsersService
+    private userService: UsersService,
+    private departmentService: DepartmentsService,
+    private formTemplatesService: FormTemplateService,
+    private projectsService: ProjectsService
   ) {
 
     this.cities = [
@@ -40,13 +55,44 @@ export class HeadOfDepartmentPageComponent implements OnInit {
     ];
   }
 
-  ngOnInit(): void {
-    this.authorizeService.getUser().subscribe((data) => {
-      var tempUser: any = data;
-      this.userService.getUserById(tempUser.sub).subscribe((result) => {
-        this.headOfDepartment = result;
-        this.departmentId = this.headOfDepartment.departmentId!;
-      });
-    });
+  ngOnInit() {   
+
+    this.departmentId = this.authorizeService.getUserDepartmentId()
+                            .pipe(tap((depId) => {                              
+                                                  if (depId)
+                                                  {
+                                                    this.department = this.departmentService.getDepartmentById(Guid.parse(depId));
+                                                    this.formTemplates = this.formTemplatesService
+                                                                            .getFormTemplates(Guid.parse(depId));
+                                                    this.projects = this.projectsService.getProjects()
+                                                                                        .pipe(filter((projectsList, index) => projectsList[index].departmentId == Guid.parse(depId)));
+                                                    this.members = this.userService.getUsersOfDepartment(Guid.parse(depId));
+                                                  }
+                                                }
+                                      )
+                                );                                          
+    
+  }
+
+  getVerifiedTemplates(templatesList: FormTemplate[] | null): FormTemplate[]
+  {
+    if (templatesList)
+      return templatesList;
+
+    return [];
+  }
+
+  getVerifiedMembers(membersList: UserDTO[]|null):UserDTO[]
+  {
+    if (membersList)
+      return membersList;
+    return [];
+  }
+
+  getVerifiedProjects(projectsList: Project[] | null):Project[]
+  {
+    if (projectsList)
+      return projectsList;
+    return [];
   }
 }
