79983ac352a21ecd68e00d8290b998f6f64d99e3
SinkyZ <SinkyZ@SINKY>
2022-10-05 10:29:06 +0300

    Add get criteria by id method

--- EmployeeEvaluation/ClientApp/src/app/components/evaluation-form/evaluation-form/criteria-details/criteria-details/criteria-details.component.ts
+++ EmployeeEvaluation/ClientApp/src/app/components/evaluation-form/evaluation-form/criteria-details/criteria-details/criteria-details.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Guid } from 'guid-typescript';
-import { Subscribable, Subscription } from 'rxjs';
+import { Observable, Subscribable, Subscription } from 'rxjs';
 import { EvaluationFormService } from 'src/app/services/evaluation-form.service';
 import {ButtonModule} from 'primeng/button';
 import {DialogModule} from 'primeng/dialog';
 import { RippleModule } from 'primeng/ripple';
+import { FormCriteria } from 'src/app/models/form-criteria.model';
 
 @Component({
   selector: 'app-criteria-details',
@@ -15,9 +16,23 @@ import { RippleModule } from 'primeng/ripple';
 })
 export class CriteriaDetailsComponent implements OnInit {
 
-  constructor(private activatedRoute: ActivatedRoute, private formEvalService: EvaluationFormService) { }
+  formCriteria!: Observable<FormCriteria>;
+  formCriteriaId: any;
+  evaluationFormId: any;
+
+  constructor(private activatedRoute: ActivatedRoute, private formEvalService: EvaluationFormService)
+   { 
+    this.activatedRoute.paramMap.subscribe((params) => {
+      this.evaluationFormId = params.get('formId');
+      this.formCriteriaId = params.get('id');
+    });
+   }
 
   ngOnInit(): void {
+    this.getFormCriteriaById();
   }
 
+  getFormCriteriaById() {
+    this.formCriteria = this.formEvalService.getFormCriteriaById(this.evaluationFormId, this.formCriteriaId);
+  }
 }
--- EmployeeEvaluation/ClientApp/src/app/services/evaluation-form.service.ts
+++ EmployeeEvaluation/ClientApp/src/app/services/evaluation-form.service.ts
@@ -6,7 +6,7 @@ import { EvaluationForm } from '../models/evaluation-form.model';
 import { FormSection } from 'src/app/models/form-section.model';
 import { FormCriteria } from 'src/app/models/form-criteria.model';
 import { Guid } from 'guid-typescript';
-import {CriteriaReview } from '../models/criteria-review.model';
+import { CriteriaReview } from '../models/criteria-review.model';
 import { Form } from '@angular/forms';
 
 
@@ -22,6 +22,9 @@ export class EvaluationFormService {
   public getEvaluationForms(id: Guid): Observable<EvaluationForm> {
     return this.httpClient.get<EvaluationForm>(`${environment.apiUrl}/${this.url}/${id}`);
   }
+  public getFormCriteriaById(formId: Guid, id: Guid): Observable<FormCriteria> {
+    return this.httpClient.get<FormCriteria>(`${environment.apiUrl}/${this.url}/${formId}/criteria-details/${id}`)
+  }
 
   public createEvaluationForm(id: Guid, evaluationForm: EvaluationForm): Observable<EvaluationForm> {
     return this.httpClient.post<EvaluationForm>(`${environment.apiUrl}/${this.url}/${id}`, evaluationForm);
@@ -30,7 +33,7 @@ export class EvaluationFormService {
   public createCriteriaReview(id: Guid, criteriaReview: CriteriaReview): Observable<EvaluationForm> {
     return this.httpClient.post<EvaluationForm>(`${environment.apiUrl}/${this.url}/${id}/CriteriaReview`, criteriaReview);
   }
-  public updateFormCriteria(id:Guid, formCriteria:FormCriteria):Observable<EvaluationForm>{
+  public updateFormCriteria(id: Guid, formCriteria: FormCriteria): Observable<EvaluationForm> {
     return this.httpClient.put<EvaluationForm>(`${environment.apiUrl}/${this.url}/${id}/FormCriteria`, formCriteria);
   }
 }
